1f47d78989efbcd01b00d995203b24d2caa8607e
Isaac Levin <isaac.levin@cibc.com>
2015-03-09 12:43:09 -0400

    Implemented progressive drill component

--- bu.js
+++ bu.js
@@ -1,16 +1,60 @@
 var buApp = angular.module('buApp', []);
-buApp.controller('ProgressiveDrillCtrl', [function() {
-    this.startPos = 4;
-    this.maxPos = 7;
-    this.minPos = 1;
-    this.numOfShots = 10;
-    this.maxScore = 10;
-    this.shots = [];
+buApp.directive('progressiveDrill', [function() {
+    return {
+        templateUrl: 'progressive-drill-tpl.html',
+        controllerAs: 'ctrl',
+        bindToController: true,
+        controller: function($attrs, $scope) {
+            var startPos = $scope.$eval($attrs.startPos) || 4;
+            var maxPos = $scope.$eval($attrs.maxPos) || 7;
+            var minPos = $scope.$eval($attrs.minPos) || 1;
+            var numOfShots = $scope.$eval($attrs.numOfShots) || 10;
+            var maxScore = $scope.$eval($attrs.maxScore) || 10;
 
-    this.init = function () {
-        for (var i=0; i<this.numOfShots; i++) {
-            this.shots.push(this.startPos);
+            this.shots = [];
+
+            var curShot = 0;
+            var curPos = startPos;
+
+            this.init = function () {
+                for (var i=0; i<numOfShots; i++) {
+                    this.shots.push('-');
+                }
+            };
+
+            this.afterShot = function(success) {
+                if (curShot < numOfShots) {
+                    curPos = (success) ? curPos + 1 : curPos - 1;
+                    curPos = Math.max(curPos, minPos);
+                    curPos = Math.min(curPos, maxPos);
+                    this.shots[curShot] = curPos;
+                    curShot++;
+                    if (curShot == numOfShots) {
+                        this.score = this.calcScore();
+                    }
+                }
+            };
+
+            this.calcScore = function() {
+                var lastScore = 0;
+                var bonus = -1;
+                if (this.shots) {
+                    for (var i = 0; i < this.shots.length; i++) {
+                        if (this.shots[i]) {
+                            lastScore = this.shots[i];
+                            if (this.shots[i] == 7) {
+                                bonus++;
+                            }
+                        }
+                    }
+                }
+                var result = lastScore + Math.max(bonus, 0);
+                return Math.min(result, maxScore);
+            };
         }
-    };
-
+    }
 }]);
+
+buApp.controller('ProgressiveDrillTest', function() {
+
+});
