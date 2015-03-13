4628cf0670c9b885cadb37c777c855c8c9dd52ff
Isaac Levin <isaac.levin@cibc.com>
2015-03-13 10:59:06 -0400

    Moved bu-drill to separate file

--- bu.js
+++ bu.js
@@ -55,67 +55,10 @@ buApp.directive('progressiveDrill', [function() {
     }
 }]);
 
-buApp.directive('buDrill', [function () {
-    return {
-        restrict: 'EA',
-        templateUrl: 'bu-drill-tpl.html',
-        replace: true,
-        transclude: true,
-        scope: {
-            name: '@drillName',
-            shotTitle: '@',
-            successTitle: '@',
-            failureTitle: '@',
-            numOfShots: '@',
-            triesPerShot: '@',
-            score: '='
-        },
-        link: function(scope, element, attrs) {
-            if (!attrs.shotTitle) {attrs.shotTitle = 'Shot'}
-            if (!attrs.successTitle) {attrs.successTitle = 'Success'}
-            if (!attrs.failureTitle) {attrs.failureTitle = 'Failure'}
-            if (!attrs.numOfShots) {attrs.numOfShots = 10}
-            attrs.numOfShots = scope.$eval(attrs.numOfShots);
-            if (!attrs.triesPerShot) {attrs.triesPerShot = 1}
-            attrs.triesPerShot = scope.$eval(attrs.triesPerShot);
-
-            scope.range = function (n) {
-                return new Array(n);
-            };
-            var shotsNum = attrs.numOfShots * attrs.triesPerShot;
-            scope.shots = [];
-            for (var i=0; i<shotsNum; i++) {
-                scope.shots.push(null);
-            }
-
-            scope.displayShot = function (input, reverse) {
-                if (input == null) return null;
-                if (reverse) {
-                    return input == 0 ? '\u2713' : null;
-                } else {
-                    return input > 0 ? '\u2713' : null;
-                }
-            };
-
-            scope.markShot = function (index, success) {
-                scope.shots[index] = (success) ? 1 : 0;
-                scope.recalcScore();
-            };
-
-            scope.recalcScore = function () {
-                var score = 0;
-                for (var i=0; i<scope.shots.length; i++) {
-                    score += scope.shots[i];
-                }
-                scope.score = score;
-            };
-        }
-    }
-}]);
 
 buApp.controller('ProgressiveDrillTest', ['$scope', function($scope) {
     $scope.drills = [
-        {name: 'Wagon wheel', score: 0},
-        {name: 'Potting', score: 0}
+        {id: 'F6', score: 0},
+        {id: 'F7', score: 0}
     ];
 }]);
