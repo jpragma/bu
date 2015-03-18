4d256719a15b4235360ac4f81dd1e8fa85837dfd
Isaac Levin <isaac.levin@cibc.com>
2015-03-18 11:30:26 -0400

    Fixed parsing of arguments in bu-boolean directive

--- directives/bu-boolean.js
+++ directives/bu-boolean.js
@@ -11,25 +11,23 @@ buApp.directive('buBoolean', [function () {
             triesPerShot: '@',
             score: '='
         },
-        link: function(scope, element, attrs) {
-            if (!attrs.shotTitle) {attrs.shotTitle = 'Shot'}
-            if (!attrs.successTitle) {attrs.successTitle = 'Success'}
-            if (!attrs.failureTitle) {attrs.failureTitle = 'Failure'}
-            if (!attrs.numOfShots) {attrs.numOfShots = 10}
-            attrs.numOfShots = scope.$eval(attrs.numOfShots);
-            if (!attrs.triesPerShot) {attrs.triesPerShot = 1}
-            attrs.triesPerShot = scope.$eval(attrs.triesPerShot);
+        link: function($scope, $element, $attrs) {
+            $scope.shotTitle = $scope.$eval($attrs.shotTitle) || 'Shot';
+            $scope.successTitle = $scope.$eval($attrs.successTitle) || 'Success';
+            $scope.failureTitle = $scope.$eval($attrs.failureTitle) || 'Failure';
+            $scope.numOfShots = $scope.$eval($attrs.numOfShots) || 10;
+            $scope.triesPerShot = $scope.$eval($attrs.triesPerShot) || 1;
 
-            scope.range = function (n) {
-                return new Array(n);
+            $scope.range = function (n) {
+                return new Array($scope.$eval(n));
             };
-            var shotsNum = attrs.numOfShots * attrs.triesPerShot;
-            scope.shots = [];
+            var shotsNum = $scope.numOfShots * $scope.triesPerShot;
+            $scope.shots = [];
             for (var i=0; i<shotsNum; i++) {
-                scope.shots.push(null);
+                $scope.shots.push(null);
             }
 
-            scope.displayShot = function (input, reverse) {
+            $scope.displayShot = function (input, reverse) {
                 if (input == null) return null;
                 if (reverse) {
                     return input == 0 ? '\u2713' : null;
@@ -38,20 +36,20 @@ buApp.directive('buBoolean', [function () {
                 }
             };
 
-            scope.markShot = function (index, success) {
-                if (index > 0 && scope.shots[index-1] == null) {
+            $scope.markShot = function (index, success) {
+                if (index > 0 && $scope.shots[index-1] == null) {
                     return; // Previous shot is not marked yet
                 }
-                scope.shots[index] = (success) ? 1 : 0;
-                scope.recalcScore();
+                $scope.shots[index] = (success) ? 1 : 0;
+                $scope.recalcScore();
             };
 
-            scope.recalcScore = function () {
+            $scope.recalcScore = function () {
                 var score = 0;
-                for (var i=0; i<scope.shots.length; i++) {
-                    score += scope.shots[i];
+                for (var i=0; i<$scope.shots.length; i++) {
+                    score += $scope.shots[i];
                 }
-                scope.score = score;
+                $scope.score = score;
             };
         }
     }
