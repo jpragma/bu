f9f895c5fbcca0f47d8d1815ddc0c97bce200c22
Isaac Levin <isaac.levin@cibc.com>
2015-03-19 10:20:42 -0400

    Implemented break directive

---
+++ directives/bu-break.js
@@ -0,0 +1,49 @@
+buApp.directive('buBreak', [function () {
+    return {
+        restrict: 'EA',
+        templateUrl: '../tmpl/bu-break-tpl.html',
+        replace: true,
+        scope: {
+            numOfAttempts: '@',
+            score: '='
+        },
+        link: function($scope, $element, $attrs) {
+            $scope.numOfAttempts = $scope.$eval($attrs.numOfAttempts) || 3;
+
+            $scope.attempts = [];
+            for (var i=0; i<$scope.numOfAttempts; i++) {
+                var points = new Array(5);
+                $scope.attempts.push(points);
+            }
+
+            $scope.displayPoint = function (input) {
+                if (input == null) return '\u00A0'; // &nbsp
+                return input > 0 ? '\u2713' : '\u2718';
+            };
+
+            $scope.markPoint = function (attempt, index) {
+                var val = attempt[index];
+                val = (val == 1) ? 0 : 1;
+                attempt[index] = val;
+                $scope.recalcScore();
+            };
+
+            $scope.recalcScore = function () {
+                var scores = [];
+                for (var i=0; i<$scope.attempts.length; i++) {
+                    var attempt = $scope.attempts[i];
+                    var s = 0;
+                    for (var j=0; j<attempt.length; j++) {
+                        s += (attempt[j] || 0);
+                    }
+                    scores.push(s);
+                }
+                scores.sort();
+                var median = Math.floor(scores.length / 2);
+                $scope.score = scores[median];
+            };
+
+        }
+    }
+}]);
+
